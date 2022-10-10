70c5cd189b9bdb2c58285c5b135dd8b67f5d0e59
Myeongjin Kim <myungjin2009@naver.com>
2022-10-10 13:14:57 +0900

    Spring Basic Leacture 11/27 Java study : Understanding of expert Lamda Expression, basic Functional Interface Spring study : Coded Test case of MemoryMemberRepository

--- DDingJin/202210SpringPractice/src/main/java/com/syu/SpringPractice/repository/MemoryMemberRepository.java
+++ DDingJin/202210SpringPractice/src/main/java/com/syu/SpringPractice/repository/MemoryMemberRepository.java
@@ -2,10 +2,7 @@ package com.syu.SpringPractice.repository;
 
 import com.syu.SpringPractice.domain.Member;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
+import java.util.*;
 
 public class MemoryMemberRepository implements MemberRepository{
 
@@ -26,12 +23,18 @@ public class MemoryMemberRepository implements MemberRepository{
 
     @Override
     public Optional<Member> findByName(String name) {
-        store.values().stream()
-                .filter(member)
+        return store.values().stream()
+                .filter(member -> member.getName().equals(name))
+                .findAny();
     }
 
     @Override
     public List<Member> findAll() {
-        return null;
+        return new ArrayList<>(store.values());
+    }
+
+
+    public void clearStore() {
+        store.clear();
     }
 }
---
+++ DDingJin/OriginalJava_Practice/src/MyFunction2.java
@@ -0,0 +1,5 @@
+
+@FunctionalInterface
+public interface MyFunction2 {
+    int max(int a, int b);
+}
