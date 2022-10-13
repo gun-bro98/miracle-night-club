26d4d7435d2b2cdb1d5982b73663ef6a2d4bffca
Kim Myeongjin <myungjin2009@naver.com>
2022-10-13 16:58:23 +0900

    Spring Basic Lecture 20/27
    Spring study : Basic Learning of JUnit5 Tast Case Logic with training, Understanding Java Bean structure
    H2 database : Basic Learning of H2 database console

---
+++ DDingJin/202210SpringPractice/src/main/java/com/syu/SpringPractice/controller/MemberController.java
@@ -0,0 +1,16 @@
+package com.syu.SpringPractice.controller;
+
+import com.syu.SpringPractice.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MemberController {
+
+    private final MemberService memberService;
+    @Autowired
+    public MemberController(MemberService memberService) {
+        this.memberService = memberService;
+    }
+
+}
---
+++ DDingJin/202210SpringPractice/src/main/java/com/syu/SpringPractice/repository/JdbcMemberRepository.java
@@ -0,0 +1,38 @@
+package com.syu.SpringPractice.repository;
+
+import com.syu.SpringPractice.domain.Member;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.util.List;
+import java.util.Optional;
+
+public class JdbcMemberRepository implements MemberRepository{
+
+    private final DataSource dataSource;
+
+    public JdbcMemberRepository(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    @Override
+    public Member save(Member member) {
+        String sql = "insert into member(name) values(?)";
+        return null;
+    }
+
+    @Override
+    public Optional<Member> findById(Long id) {
+        return Optional.empty();
+    }
+
+    @Override
+    public Optional<Member> findByName(String name) {
+        return Optional.empty();
+    }
+
+    @Override
+    public List<Member> findAll() {
+        return null;
+    }
+}
