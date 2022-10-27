b62fa2af1a2cb9b713912194e037b408b8e34c09
Kim Myeongjin <myungjin2009@naver.com>
2022-10-28 00:46:10 +0900

    Android Project initialized, Gradle set.

---
+++ DDingJin/202211AndroidCapstoneProject/app/src/main/java/com/capstone/oneday/MainActivity.java
@@ -0,0 +1,14 @@
+package com.capstone.oneday;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
