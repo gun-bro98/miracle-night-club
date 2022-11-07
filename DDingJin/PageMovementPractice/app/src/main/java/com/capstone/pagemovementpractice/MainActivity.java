2a7d5f073c7f09f0e17f2edd5494ec983b86b971
Kim Myeongjin <myungjin2009@naver.com>
2022-11-08 01:31:10 +0900

    Android Practice, Learning how to movement page

---
+++ DDingJin/PageMovementPractice/app/src/main/java/com/capstone/pagemovementpractice/MainActivity.java
@@ -0,0 +1,26 @@
+package com.capstone.pagemovementpractice;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button1 = (Button) findViewById(R.id.button);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), SubActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
